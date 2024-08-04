da4c289f4904b987f2844bb4ea6eb96e4780050b
accessalok <access.alok@gmail.com>
2024-08-04 09:24:58 +0530

    Initial commit

---
+++ test_model.py
@@ -0,0 +1,10 @@
+import unittest
+from model import train_model
+
+class TestModel(unittest.TestCase):
+    def test_train_model(self):
+        model = train_model()
+        self.assertIsNotNone(model)
+
+if __name__ == '__main__':
+    unittest.main()
